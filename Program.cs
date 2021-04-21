71ed733ce0039f349ff9aa18e801b02f59c4280a
Saravanakumar Rajavel <saravana.rajavel@disys.com>
2021-04-21 17:34:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.DirectoryServices.Protocols;
+using System.Net;
+
+namespace AdAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string domainName = "disys.in"; //this should be moved to config file
+                string userName = "saravana.kumar"; //put your username here
+                string password = "********"; //put your password here
+
+                //This will validate the user
+                LdapConnection connection = new LdapConnection(domainName);
+                NetworkCredential credential = new NetworkCredential(userName, password);
+                connection.Credential = credential;
+                connection.Bind(); //this line will throw exception if credentails are not valid
+
+                //Next step is the get additional details of the user
+                UserDetails userInfo = new UserDetails();
+                userInfo = userInfo.GetDetails(domainName, userName, password);
+
+                Console.WriteLine("Logged in User: {0} \nFirst Name: {1}\nLast Name: {2}",
+                    userInfo.DisplayName, userInfo.FirstName, userInfo.LastName);
+
+                Console.ReadLine();
+            }
+            catch (LdapException ldapException)
+            {
+                String error = ldapException.ServerErrorMessage;
+                Console.WriteLine(ldapException);
+                //525​ user not found ​(1317)
+                //52e​ invalid credentials ​(1326)
+                //530​ not permitted to logon at this time​ (1328)
+                //531​ not permitted to logon at this workstation​ (1329)
+                //532​ password expired ​(1330)
+                //533​ account disabled ​(1331)
+                //701​ account expired ​(1793)
+                //773​ user must reset password (1907)
+                //775​ user account locked (1909)
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ UserDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace AdAuthentication
+{
+    public class UserDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+
+        public UserDetails GetDetails(string domainName, string userName, string password)
+        {
+            UserDetails user = null;
+            try
+            {
+                DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://" + domainName, userName, password);
+                DirectorySearcher directorySearch = new DirectorySearcher(directoryEntry);
+                directorySearch.Filter = "(samAccountName=" + userName + ")";
+                SearchResult searchResult = directorySearch.FindOne();
+                DirectoryEntry result = searchResult.GetDirectoryEntry();
+
+                /*
+                 * User attributes inside active directory
+                 * http://www.kouti.com/tables/userattributes.htm
+                 * 1. However, DISYS has only stored 33 attributes and the rest like Title, Designation, Manager, Email, Mobile
+                 * are stored only in Zoho People
+                 * 2. So, we must identify an alternate way to store this information in the backend of this application
+                 * and retrieve when needed
+                 */
+                if (result != null)
+                {
+                    user = new UserDetails()
+                    {
+                        FirstName = result.Properties["givenName"].Value.ToString(),
+                        LastName = result.Properties["sn"].Value.ToString(),
+                        DisplayName = result.Properties["displayName"].Value.ToString(),
+                    };
+                }
+
+                return user;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return user;
+            }
+        }
+    }}
